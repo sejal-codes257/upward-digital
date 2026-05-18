86e4ab8e0d79ec35a22d3a70f076ba0cdfee6b92
sejal-codes257 <sejalkanwar2008@gmail.com>
2026-05-19 04:25:04 +0530

    Delete src directory

--- src/components/ui/SectionLabel.jsx
+++
@@ -1,8 +0,0 @@
-export default function SectionLabel({ children, className = '' }) {
-  return (
-    <div className={`flex items-center gap-4 ${className}`}>
-      <div className="w-8 h-px bg-beige" />
-      <span className="label-text">{children}</span>
-    </div>
-  );
-}
